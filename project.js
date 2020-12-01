b5a8b2493ce436aaf7fd97ecd454c11acebe9cc8
Glorianv <71630529+Glorianv@users.noreply.github.com>
2020-12-01 19:51:35 +0300

    send project work

---
+++ project.js
@@ -0,0 +1,14 @@
+let users = [];
+const fetchUsers = async()=> {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = awaitres.json();
+    for (let user of users){
+        createUserElement(user);
+    }
+};
+// fetch("https://jsonplaceholder.typicode.com/users").then((res) => res.json()).then((users) => {
+// for (let user of users){
+//     createUserElement(user);
+// }
+fetchUsers();
+console.log(users);
