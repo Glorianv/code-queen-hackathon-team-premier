709014e951c4debec300d23c5dc4a6d9b8c32f20
Achieng Christine <ok.tina88@gmail.com>
2020-12-02 20:19:10 -0800

    save changes

---
+++ mentorID.js
@@ -0,0 +1,79 @@
+let studentList= []
+
+    
+
+// document.getElementById("demo").innerHTML = studentList.fullName + "," + studentList.Age + "," + studentList.project +"," +stundentlist.mentorComent;
+// let ownstudentList = {name:"John", age:50, city:"New York"};
+// var myArray = Object.values(student);
+// document.getElementById("demo").innerHTML = myArray;
+
+
+
+logInMentorBtn .addEventListener("click",) => {
+    document .getElementById("load")style.display ="table";
+    let userName = document .getElementById("user").value
+    let passWord = document .getElementById ("password").value;
+
+    fetch ("http:....",{
+        method: "POST",
+        headers:{
+            "content-type":"application/json"
+        },
+        body:JSON.stringify(userName:user,passWord:password,)
+    })
+    .then({res} => Response.JSON())
+    .then (function(data){
+        userinfo = data;
+        let message!= data[message];
+        if (message! = "user logged in successfully"){
+        let info = $(data ["message"]);
+        Show (info);
+        return 0,
+        }
+        if payload_info
+    }
+
+
+    let student = {
+        firstName: "",
+        lastName : "",
+        id     : 5566,
+        fullName : function() {
+          return this.firstName + " " + this.lastName;
+        }
+      };
+      
+      document.getElementById("demo").innerHTML = student.fullName;
+      
+       student.name = {fullName:""};
+       function createTable()
+
+    const createTable =document .getElementById("table-boardered")
+    let x = document.createElement("TBODY");
+    let y = document.createElement("TR");
+    let z = document.createElement("TD");
+    z.innerHTML = "A table cell inside a tr inside a tbody inside a table!"
+
+    let num_rows = document.getElementById('rows').value;
+    let num_cols = document.getElementById('cols').value;
+    let theader = '<table border="1">\n';
+    let tbody = '';
+
+    for( let i=0; i<num_rows;i++)
+    {
+        tbody += '<tr>';
+        for( let j=0; j<num_cols;j++)
+        {
+            tbody += '<td>';
+            tbody += 'Cell ' + i + ',' + j;
+            tbody += '</td>'
+        }
+        tbody += '</tr>\n';
+    }
+    let tfooter = '</table>';
+    document.getElementById('wrapper').innerHTML = theader + tbody + tfooter;
+}
+    y.appendChild(z);
+    x.appendChild(y);
+    document.getElementById("myTable").appendChild(x);
+  }
--- studentlist.js
+++ studentlist.js
@@ -1,116 +1,68 @@
-let students = [
-    {
-        id:1,
-        firstname:"christine",
-        lastname:"Achieng",
-        age:"26",
-        email:"okechomanda105@gmail.com",
-    },
-    {
-        id:2,
-        firstname:"vero",
-        lastname:"mwaura",
-        age:"21",
-        email:"okech@gmail.com",
-    },
-    {
-        id:2,
-        firstname:"jack",
-        lastname:"kibaki",
-        age:"28",
-        email:"marlinkair@gmail.com",
-
-    },
-]
+let studentsProfile = []
 
 
-const saveBtn=document.getElementById('save-btn')
-const studentForm=document.getElementById('register')
 
-saveBtn.addEventListener('click', (e)=>{
-    e.preventDefault();
-    const firstNameInput=firstName.value;
-    const lastNameInput=lastName.value;
-    const emailInput=email.value;
-    const ageInput=age.value;
-
-    if (firstNameInput=="" ||lastNameInput=="" ||emailInput==""|| ageInput==""){
-
-        errorMessage = ("invalid")
-
-   }else{
-    let newStudent ={
-        userid:students.length+1,
-        fname:firstNameInput,
-        lname:lastNameInput,
-        email:emailInput,
-        age:ageInput
+let fetchAllUsersData => {
+    const res = await fetch( https:...)
+    const UserData = await res.json();
+    for (let user of usersData){
+        
     }
-    students.push(newStudentData)
-    const table=document.getElementById('table')
+}
+const userProfile =document.get getElementById("register")
 
-    let studentsobject;
-for(let student of students) {
-    studentObject += `<tr>`;
+const registerSaveBtn = document.get getElementById("regbtn")
 
-for(let student of students){
-    studentObject += `<td>${students[student][eachStudent]}</td>`;
-         const table=document.getElementById('table');
-         const studentForm = document.getElementById('results');
+        registerSaveBtn.addeventListener("click", async ()=> {
+            let userProfileValue = UsersDataInput.value;
+            if (userProfileValue ==="");{
+                alert("A field is required to be filled")
+            }else{
+                const usersDataPayLoad ={
+                    user:userProfileValue,
+                },
+                const response = await fetch( http:...)
+                method "POST"
+                body: JSON.stringify(usersDataPayLoad),
+                Headers:{
+                    "content type":"application/json"
+                },
+            },
+        }),
+        const user = await response.json(
+            create usersDataElement(user)
+            userDataProfile.value ="";
+        )
+        });
 
-         let email = document.getElementById("email").value.indexOf("@");
-         let age = document.getElementById("age").value;
-         let fName = document.getElementById("firstName").value;   
-         Submitted = "true";
-         let lName = document.getElementById("firstName").value;   
-         Submitted = "true";
-         if (fName.length > 10) {
-             alert("The name must have no more than 10 characters");  
-             Submitted = "false";
-        if (lName.length > 10) {
-                alert("The name must have no more than 10 characters");  
-                Submitted = "false";
-         }
-         if (isNaN(age) || age < 12 || age > 100) {  
-             alert("The age must be between numbers 12 and 100");  
-             Submitted = "false";
-         }
-         if (at == -1) {  
-             alert("The email address you entered is not valid"); 
-             Submitted = "false";
-         }
-         if (Submitted == "false") {  
-             return false;
-         }
+        
+//     {
+//         id:1,
+//         firstname:"christine",
+//         lastname:"Achieng",
+//         age:"26",
+//         email:"okechomanda105@gmail.com",
+//     },
+//     {
+//         id:2,
+//         firstname:"vero",
+//         lastname:"mwaura",
+//         age:"21",
+//         email:"okech@gmail.com",
+//     },
+//     {
+//         id:2,
+//         firstname:"jack",
+//         lastname:"kibaki",
+//         age:"28",
+//         email:"marlinkair@gmail.com",
+
+//     },
+// ]
 
 
-// let studenthubsobject;
-// for(let studenthub of studenthubs) {
-//     studenthubObject += `<tr>`;
-
-// for(let studenthub of studenthubs){
-//     studentObject += `<td>${studenthubs[studenthub][eachStudenthub]}</td>`;
-//          const table=document.getElementById('table');
-//          const studentForm = document.getElementById('results');
-    
-// }
-
-// let studenthubs= newStudenthubs;
-// function localStorageLoad(){
-//     if(localStorage.studenthubData){
-//         studenthubsArray=JSON.parse(localStorage.newStudenthubsData)
-//         let newStudenthubs = "";
-//         for(let newStudenthub of newStudenthubs) {
-//             studenthubObject += `<tr>`;
-//             for(let eachStudenthubElement of newStudenthub[studenthub]) {
-//                 studenthubObject += `<td>${newStudenthubs[studenthub][eachStudenthubElement]}</td>`;
-//             }
-//             studenthubObject += `</tr>`;
-//         }
-//          table.innerHTML = studenthubObject;
-//     }
-
-//     document.getElementById('tableresult').innerHTML = studentObject;
+// const saveBtn=document.getElementById('save-btn')
+// const studentForm=document.getElementById('register')
 
 // saveBtn.addEventListener('click', (e)=>{
 //     e.preventDefault();
@@ -124,43 +76,47 @@ for(let student of students){
 //         errorMessage = ("invalid")
 
 //    }else{
-//     var newStudenthub ={
+//     let newStudent ={
 //         userid:students.length+1,
 //         fname:firstNameInput,
 //         lname:lastNameInput,
 //         email:emailInput,
 //         age:ageInput
 //     }
-//     students.push(newStudenthubsData)
+//     students.push(newStudentData)
 //     const table=document.getElementById('table')
 
-//     const studentObject = "";
-//     for(let studenthub of studenthubs) {
-//         studenthubObject += `<tr>`;
-//         for(let eachStudenthub of studenthubs[studenthub]) {
-//             studenthubObject += `<td>${studenthubs[studenthub][eachStudenthub]}</td>`;
-//         }
-//         studenthubObject += `</tr>`;
-//         errorMessage.innerText='check your input data!!!';
-//         setTimeout(() => {   
-//             errorMessage.innerHTML=' ';
-//         }, 5000);       
-//     }
-//     else{  const studentForm=document.getElementById('register')
-//         const newStudenthub={userid:newStudenthubs.length+1, fname:firstNameInput, lname:lastNameInput, email:emailInput, age:ageInput};
-//         newStudenthubs.push(newStudenthubobject);
-//         localStorage.studenthubsdata=JSON.stringify(newStudenthubs);
-//         studentForm.reset();
-//         localStorageLoad(newStudenthubObject)
-//     }
-//     table.innerHTML = newStudenthubObject;
-//     studentForm.reset();
+//     let studentsobject;
+// for(let student of students) {
+//     studentObject += `<tr>`;
 
-// }
+// for(let student of students){
+//     studentObject += `<td>${students[student][eachStudent]}</td>`;
+//          const table=document.getElementById('table');
+//          const studentForm = document.getElementById('results');
 
-// })
+//          let Email = document.getElementById("email-holder").value.indexOf("@");
+//          let age = document.getElementById("age-holder").value;
+//          let fName = document.getElementById("givenName-input").value;   
+//          Submitted = "true";
+//          let lName = document.getElementById("thename-input").value;   
+//          Submitted = "true";
+//          if (fName.length > 10) {
+//              alert("The name must have no more than 10 characters");  
+//              Submitted = "false";
+//         if (lName.length > 10) {
+//                 alert("The name must have no more than 10 characters");  
+//                 Submitted = "false";
+//          }
+//          if (isNaN(age) || age < 12 || age > 100) {  
+//              alert("The age must be between numbers 12 and 100");  
+//              Submitted = "false";
+//          }
+//          if (at == -1) {  
+//              alert("The email address you entered is not valid"); 
+//              Submitted = "false";
+//          }
+//          if (Submitted == "false") {  
+//              return false;
+//          }
 
-// const clearError=document.getElementById('savebtn')
-// clearError.addEventListener("click",()=>{
-
-// })
